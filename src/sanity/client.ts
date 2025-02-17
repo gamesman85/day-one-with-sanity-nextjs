0bda5170df6bd7464afa404dbbd626f6269d2ea9
Daniel Wang <daniel.wang@try.no>
2025-02-17 15:16:10 +0100

    configure sanity client for fetching + create live fetch utilities

---
+++ src/sanity/client.ts
@@ -0,0 +1,9 @@
+import { createClient } from "next-sanity";
+
+export const client = createClient({
+  projectId: "4uzecwmq",
+  dataset: "production",
+  apiVersion: "2024-11-01",
+  useCdn: false,
+});
+
